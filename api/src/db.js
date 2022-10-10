0b2f452e957d592b061858d34a92f910d6359eb6
Kristhian92 <88893885+Kristhian92@users.noreply.github.com>
2022-10-09 23:35:49 -0500

    Deploy Railway

--- api/src/db.js
+++ api/src/db.js
@@ -13,7 +13,7 @@ let sequelize =
       database: DB_NAME,
       dialect: "postgres",
       host: DB_HOST,
-      port: 5945,
+      port: 5432,
       username: DB_USER,
       password: DB_PASSWORD,
       pool: {
